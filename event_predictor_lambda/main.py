e53a895b3fc640bd05208064a0793ae468d37b78
AndrewKehl <60129754+AndrewKehl@users.noreply.github.com>
2023-05-06 00:57:43 -0700

    model notebook load, empty main and requirements for event predictions

---
+++ event_predictor_lambda/main.py
@@ -0,0 +1,7 @@
+# Once every five minutes
+#Poll for games (date, hometeam, awayteam, game id)
+#Use list of game_id to check for events:
+# predict on those events
+#insert predictions into new db table
+#pull data into the flask app
+
