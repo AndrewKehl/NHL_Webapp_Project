14bbfe6270110d562ced8d49c33e540d3239334d
AndrewKehl <60129754+AndrewKehl@users.noreply.github.com>
2023-05-07 20:44:49 -0700

    rolledback

--- main.py
+++ main.py
@@ -1,54 +1,64 @@
-import os
-import pandas as pd
-import boto3
-from functools import wraps
 import dash
-import dash_table
-import dash_core_components as dcc
-import dash_html_components as html
+import boto3
+import os
+from flask import Flask, redirect, url_for, session, render_template
+from functools import wraps
+import plotly.express as px
+from dash import dcc, html
 from dash.dependencies import Input, Output
 from authlib.integrations.flask_client import OAuth
-from flask import Flask, redirect, url_for, session, render_template
+import pandas as pd
+import requests
+
+# Load Iris dataset
+df = px.data.iris()
 
 # Connect to DynamoDB
 dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
-prediction_table = dynamodb.Table('predicted_events')
-teams_table = dynamodb.Table('nhl_teams')
-
-predict_response = prediction_table.scan()
-predict_items = predict_response['Items']
-
-teams_response = teams_table.scan()
-teams_items = teams_response['Items']
-
-predicted_events_df = pd.DataFrame(predict_items)
-nhl_teams_df = pd.DataFrame(teams_items)
-
-# Get the highest eventIdx for each game
-max_events_df = predicted_events_df.groupby('Game_ID', as_index=False)['eventIdx'].max()
-
-# Merge the highest eventIdx data with the original dataframe
-merged_df = pd.merge(max_events_df, predicted_events_df, on=['Game_ID', 'eventIdx'], how='left')
-
-# Replace team IDs with team abbreviations
-merged_df['home'] = merged_df['home'].apply(lambda x: nhl_teams_df.loc[nhl_teams_df['Team_ID'] == x, 'abbreviation'].values[0])
-merged_df['away'] = merged_df['away'].apply(lambda x: nhl_teams_df.loc[nhl_teams_df['Team_ID'] == x, 'abbreviation'].values[0])
+schedulesTable = dynamodb.Table('nhl-schedules')
 
 # Initialize the server
 server = Flask(__name__)
 server.secret_key = os.environ.get('SERVER_SECRET_KEY', 'secret-key')
+# server.config['SERVER_NAME'] = 'localhost:8050'
 
 oauth = OAuth(server)
 app = dash.Dash(__name__, server=server, url_base_pathname='/dash/')
 
 # Define the layout of the app
 app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
+    dcc.Location(id='url', refresh=False),  # this locates this structure to the url
     html.Div(id='page-content'),
-    html.H1('NHL Predicted Events'),
-
-    html.Div(id='table-container')
+    html.H1('NHL Schedules'),
+    html.Ul(id='schedule-list'),
+    html.H1('Iris Dataset'),
+    dcc.Graph(
+        id='scatter-plot',
+        figure={
+            'data': [
+                {'x': df[df['species'] == 'setosa']['sepal_width'], 'y': df[df['species'] == 'setosa']['sepal_length'],
+                 'type': 'scatter', 'mode': 'markers', 'name': 'Setosa'},
+                {'x': df[df['species'] == 'versicolor']['sepal_width'],
+                 'y': df[df['species'] == 'versicolor']['sepal_length'], 'type': 'scatter', 'mode': 'markers',
+                 'name': 'Versicolor'},
+                {'x': df[df['species'] == 'virginica']['sepal_width'],
+                 'y': df[df['species'] == 'virginica']['sepal_length'], 'type': 'scatter', 'mode': 'markers',
+                 'name': 'Virginica'}
+            ],
+            'layout': {
+                'title': 'NHL stuff continuously deployed',
+                'xaxis': {'title': 'Sepal Width'},
+                'yaxis': {'title': 'Sepal Length'}
+            }
+        }
+    ),
+    dcc.Interval(
+        id='interval-component',
+        interval=1 * 1000,  # in milliseconds
+        n_intervals=0
+    )
 ])
+
 
 def login_required(f):
     @wraps(f)
@@ -63,6 +73,9 @@ def login_required(f):
 @server.route('/google/')
 def google():
     # Google Oauth Config
+    # Get client_id and client_secret from environment variables
+    # For developement purpose you can directly put it
+    # here inside double quotes
     GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
     GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
 
@@ -81,18 +94,54 @@ def google():
     redirect_uri = url_for('google_auth', _external=True)
     return oauth.google.authorize_redirect(redirect_uri)
 
+
 @server.route('/google/auth/')
 def google_auth():
     token = oauth.google.authorize_access_token()
     session['user'] = token['userinfo']
     return redirect('/')
 
+
 @server.route('/')
 @login_required
 def default_path():
     return redirect("/dash/", code=302)
 
+
+@server.route('/quack')
+def quack():
+    return 'hello'
+
 @server.route('/logout')
 def logout():
     session.pop('user', None)
-    return
+    return redirect('/')
+
+# Define app callback
+@app.callback(Output('schedule-list', 'children'),
+              Input('interval-component', 'n_intervals'))
+def update_schedule(n):
+    global schedulesTable
+
+    # Retrieve all items from DynamoDB table
+    response = schedulesTable.scan()
+    items = response.get('Items')
+
+    # Create list of items
+    schedule_list = [
+        html.Li(f"{item['home_team']} vs {item['away_team']} on {item['date']}")
+        for item in items
+    ]
+
+    return schedule_list
+
+
+@app.callback(Output('page-content', 'children'), #this changes the content
+              [Input('url', 'pathname')]) #this listens for the url in use
+def display_dash(pathname):
+    user = dict(session).get('user', None)
+    if user is None:
+        return (dcc.Location(id="redirect", pathname="/"))
+
+if __name__ == '__main__':
+    app.run_server(host='0.0.0.0', debug=True)
